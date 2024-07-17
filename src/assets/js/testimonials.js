9c1b0f40e078f10505329aaf850702a00fd1cbb7
bittarov <81069087+bittarwork@users.noreply.github.com>
2024-07-17 14:13:51 +0300

    a7a

--- src/assets/js/testimonials.js
+++ src/assets/js/testimonials.js
@@ -1,22 +1,25 @@
-import BasePage from './base-page';
+import BasePage from "./base-page";
 
 class Testimonials extends BasePage {
-    onReady() {
-        let commentsList = app.element('salla-comments');
+  onReady() {
+    let commentsList = app.element("salla-comments");
 
-
-        let urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.has('sort')) {
-            app.element('#testimonials-filter').value = urlParams.get('sort');
-        }
-
-        app.on('change', '#testimonials-filter', async (event) => {
-            window.history.replaceState(null, null, salla.helpers.addParamToUrl('sort', event.currentTarget.value));
-            commentsList.sort = event.currentTarget.value;
-            await commentsList.reload();
-            commentsList.setAttribute('sort', event.currentTarget.value)
-        });
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("sort")) {
+      app.element("#testimonials-filter").value = urlParams.get("sort");
     }
+
+    app.on("change", "#testimonials-filter", async (event) => {
+      window.history.replaceState(
+        null,
+        null,
+        salla.helpers.addParamToUrl("sort", event.currentTarget.value)
+      );
+      commentsList.sort = event.currentTarget.value;
+      await commentsList.reload();
+      commentsList.setAttribute("sort", event.currentTarget.value);
+    });
+  }
 }
 
-Testimonials.initiateWhenReady(['testimonials']);
+Testimonials.initiateWhenReady(["testimonials-enhanced"]);
