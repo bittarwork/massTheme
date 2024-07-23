ffa37eb378313c2bfe97ce11cc46de447c42bc18
bittarov <81069087+bittarwork@users.noreply.github.com>
2024-07-23 12:29:24 +0300

    a1852a

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     "wishlist-card": asset("js/partials/wishlist-card.js"),
     checkout: [asset("js/cart.js"), asset("js/thankyou.js")],
     pages: [asset("js/loyalty.js"), asset("js/brands.js")],
-    product: [asset("js/product.js"), asset("js/products.js"), asset("js/CustomProductCard.js")], 
+    product: [asset("js/product.js"), asset("js/products.js")],
     order: asset("js/order.js"),
     testimonials: asset("js/testimonials.js"),
   },
